ce2443a2e4bf20fd9337b630663c1ac494b93df4
Stochastic13 <28253514+Stochastic13@users.noreply.github.com>
2021-02-08 08:39:38 +0530

    Updated data
    Updated the MODELLER data, added post-processing. Updated EM data, added GMM scripts and data. Updated all READMEs.

---
+++ data/em/gmm_selection.bat
@@ -0,0 +1,1 @@
+python gmm_selection.py gmm/full_PMremoved.mrc 30 gmm/full_PMremoved_gmm_ -s 0.006 -i 100000 -n 1000000 -m gmm/full_PMremoved_ >> gmm_selection1.log
---
+++ data/homology/post_processing.bat
@@ -0,0 +1,7 @@
+python scripts/pdb_postprocessing.py output_data/models_selected/pkp2a.pdb A 350 output_data/models_selected/pkp2a_modified.pdb 19 output_data/modeller_alignment_files/alignment_MODELLER_pkp2a-1xm9.ali
+python scripts/pdb_postprocessing.py output_data/models_selected/pkp3a.pdb A 319 output_data/models_selected/pkp3a_modified.pdb 19 output_data/modeller_alignment_files/alignment_MODELLER_pkp3a-1xm9.ali
+
+python scripts/pdb_postprocessing.py output_data/models_selected/pg-dsg3-pg-dsg3.pdb A A:125,B:-421,C:-311,D:-405 output_data/models_selected/pg-dsg3-pg-dsg3_modified.pdb
+python scripts/pdb_postprocessing.py output_data/models_selected/pg-dsg1-pg-dsg1.pdb A A:125,B:-421,C:-383,D:-451 output_data/models_selected/pg-dsg1-pg-dsg1_modified.pdb
+python scripts/pdb_postprocessing.py output_data/models_selected/pg-dsc3-pg-dsc3.pdb A A:125,B:-421,C:-261,D:-338 output_data/models_selected/pg-dsc3-pg-dsc3_modified.pdb
+python scripts/pdb_postprocessing.py output_data/models_selected/pg-dsc1-pg-dsc1.pdb A A:125,B:-421,C:-247,D:-308 output_data/models_selected/pg-dsc1-pg-dsc1_modified.pdb
